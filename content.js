5701b696f29dd75884ec499e9c962605bea14db4
Matthew <64444406+Mcaplovitz@users.noreply.github.com>
2022-06-28 20:46:47 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,17 @@
 // content.js
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        console.log("worked.");
-
-    }
-);
+import {PythonShell} from 'python-shell';
+let options = {
+    args: (window.location.href, "test")
+};
+document.addEventListener('DOMContentLoaded', function () { //loads script when button pressed
+    var checkPageButton = document.getElementById('checkPage');
+    var page;
+    checkPageButton.addEventListener('click', function () { //watches for the button press 
+        console.log("The URL of this page is: " + window.location.href)   
+        // page = window.location.href
+        // PythonShell.run('webscrape.py', options, function (err) {
+        //     if (err) throw err;
+        //     console.log('finished');
+        // });
+    }, false);
+}, false);
