e98c1689b759605426be166d7e90770d0f43b2e7
Matthew <64444406+Mcaplovitz@users.noreply.github.com>
2022-06-28 20:22:36 -0500

    trying to add python script to webscrape images

--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
 //get contents of page
-
+import {PythonShell} from 'python-shell';
+let options = {
+    args: (window.location.href, "test")
+};
 document.addEventListener('DOMContentLoaded', function () { //loads script when button pressed
     var checkPageButton = document.getElementById('checkPage');
-    var pageContents;
+    var page;
     checkPageButton.addEventListener('click', function () { //watches for the button press 
-        console.log("The URL of this page is: " + window.location.href)   
-
+        // console.log("The URL of this page is: " + window.location.href)   
+        page = window.location.href
+        PythonShell.run('webscrape.py', options, function (err) {
+            if (err) throw err;
+            console.log('finished');
+        });
     }, false);
 }, false);
 
