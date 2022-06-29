760208846f3a3ab76590b37a081668a0f4faf93e
Matthew <64444406+Mcaplovitz@users.noreply.github.com>
2022-06-28 20:45:26 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,7 @@
 // content.js
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        if (request.message === "clicked_browser_action") {
-            var firstHref = $("a[href^='http']").eq(0).attr("href");
+        console.log("worked.");
 
-            console.log("worked.");
-        }
     }
 );
