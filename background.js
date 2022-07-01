4084060504b2938b3f05b7caa0dbee181001236b
Matthew <mcap@brandeis.edu>
2022-07-01 16:44:54 -0500

    Removed some modules, and updated background.js and content.js
    Trying a new method of web scraping where the background sends a message to the content script in order to read the page, but the extension throws an error about an uncaught promise. Will need to do more research.

--- background.js
+++ background.js
@@ -1,11 +1,35 @@
 // background.js
 
 // Called when the user clicks on the browser action.
-chrome.action.onClicked.addListener(tab => {
-    // Send a message to the active tab
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+// chrome.action.onClicked.addListener(tab => {
+//     // Send a message to the active tab
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//         var activeTab = tabs[0];
+//         chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
+//         //test 2
+//     });
+// });        
+
+// Run script each time Chrome extension icon clicked
+document.addEventListener("DOMContentLoaded", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
         var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
-        //test 2
+        chrome.tabs.sendMessage(activeTab.id, {
+            message: "FETCH_IMAGE"
+        });
     });
-});        
+});
+chrome.runtime.onMessage.addListener((request, sender) => {
+    if (request.message === "SHOW_RESULTS") {
+        const status = request.results.imageSource.length;
+        const app = document.querySelector('.result-items');
+
+        // Check if array is empty to prevent error
+        if (status) {
+            app.innerHTML = request.results.imageSource
+                .map(img => `
+         <a href="${img}" target="_blank"><img src="${img}"/></a>
+        `).join(" ");
+        }
+    }
+});
--- content.js
+++ content.js
@@ -1,17 +1,30 @@
 // content.js
-import {PythonShell} from 'python-shell';
-let options = {
-    args: (window.location.href, "test")
-};
-document.addEventListener('DOMContentLoaded', function () { //loads script when button pressed
-    var checkPageButton = document.getElementById('checkPage');
-    var page;
-    checkPageButton.addEventListener('click', function () { //watches for the button press 
-        console.log("The URL of this page is: " + window.location.href)   
-        page = window.location.href
-        PythonShell.run('webscrape.py', options, function (err) {
-            if (err) throw err;
-            console.log('finished');
+// document.addEventListener('DOMContentLoaded', function() { //loads script when button pressed
+//     var checkPageButton = document.getElementById('checkPage');
+//     var page;
+//     checkPageButton.addEventListener('click', function() { //watches for the button press 
+//         console.log("The URL of this page is: " + window.location.href)
+//             // page = window.location.href
+
+//     }, false);
+// }, false);
+
+chrome.runtime.onMessage.addListener((request, sender) => {
+    if (request.message === "FETCH_IMAGE") {
+        console.log("The URL of this page is: " + window.location.href)
+            // Get images from product page/gallery
+        const imageContainer =
+            document.querySelectorAll(".ad-image-wrapper .ad-image img") ||
+            document.querySelectorAll("imagebox-thumbnail img");
+        // Make array from NodeList object
+        const imageArray = Array.from(imageContainer);
+        // Get image source 
+        const imageSource = imageArray.length && imageArray
+            .map(src => src.getAttribute("src"));
+        // Sending message with results   
+        chrome.runtime.sendMessage({
+            message: "SHOW_RESULTS",
+            results: { imageSource: imageSource }
         });
-    }, false);
-}, false);
+    }
+});
